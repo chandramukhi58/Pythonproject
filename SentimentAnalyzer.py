45ca114434240c95665bcafd4814c2fa71770fd2
Valeti Sai Lakshmi Chandramukhi <valetichandramukhi@gmail.com>
2025-10-30 22:21:54 +0530

    SentimentAnalyzer.py

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -1,11 +1,13 @@
 import PyPDF2
 import docx
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from textblob import TextBlob
 from collections import Counter
 from nltk.corpus import stopwords
 import nltk
 import string
 nltk.download('stopwords')
+nltk.download('vader_lexicon')
 file=input("Enter file name(.pdf/.docx/.txt):")
 text=""
 if file.endswith(".pdf"):
@@ -33,16 +35,19 @@ for ch in text:
     if ch not in string.punctuation:
         cleantext+=ch
 cleantext=cleantext.replace(" "," ")
-text=cleantext.strip()
-b=TextBlob(text)
-polarity=b.sentiment.polarity
-subjectivity=b.sentiment.subjectivity
-if polarity>0:
+cleantext=cleantext.strip()
+analyzer=SentimentIntensityAnalyzer()
+vader=analyzer.polarity_scores(cleantext)['compound']
+if vader>=0.05:
     sentiment="Positive"
-elif polarity<0:
+elif vader<=-0.05:
     sentiment="Negative"
 else:
     sentiment="Neutral"
+b=TextBlob(text)
+polarity=b.sentiment.polarity
+subjectivity=b.sentiment.subjectivity
+
 words=text.split()
 sentences=text.split('.')
 sen=[]
@@ -64,15 +69,15 @@ for w in words:
         filter.append(w)
 freq=Counter(filter)
 topmost=freq.most_common(10)
-searchword=input("enter word to search:").lower()
-wordoccurences=words.count(searchword)
 print("Sentiment:",sentiment)
 print("Polarity:",polarity)
 print("Subjectivity:",subjectivity)
 print("Total Words:",totalwords)
 print("Total Sentences:",totalsentences)
-print("Total Pragraghs:",totalparagraphs)
+print("Total Paragraphs:",totalparagraphs)
 print("Most Frequent words:")
 for w,c in topmost:
     print(w,":",c)
+searchword=input("enter word to search:").lower()
+wordoccurences=words.count(searchword)
 print("Searchword Frequency:",wordoccurences)
